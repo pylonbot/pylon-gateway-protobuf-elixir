32e73da4b7f51aab08a6ef38f562c588d631c852
Spencer Sharkey <spencer@sf-n.com>
2021-01-26 05:24:56 +0000

    ci: update install script

--- .github/update.sh
+++ .github/update.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
-echo "> installing elixir"
-sudo apt-get install -y elixir
+echo "> installing elixir + protbuf-compiler"
+sudo apt-get install -y elixir protobuf-compiler
 
 echo "> installing protoc"
 export GO111MODULE=on
-go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
-go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
-go get -u google.golang.org/grpc
 go install github.com/golang/protobuf/{proto,protoc-gen-go}
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 echo "> installing protoc-gen-elixir"
 mix escript.install hex protobuf --force
